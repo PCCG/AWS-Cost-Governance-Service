7db82ae59acf534409604ea0a770a61ce9fb21d4
Ajith Kumar S <aka434112@gmail.com>
2020-03-31 00:00:04 +0530

    fetchBucketsAcrossAllRegions now returns the region a bucket is present in

--- services/awsSimpleStorageServiceSvc.js
+++ services/awsSimpleStorageServiceSvc.js
@@ -5,15 +5,22 @@ module.exports = {
     fetchBucketsAcrossAllRegions: async () => {
         const s3 = AWS.createNewS3Object();
         const tagSetKey = 'TagSet';
+        const bucketRegionKey = 'Region';
+        const awsResponseBucketRegionKey = 'LocationConstraint';
         let listOfBuckets = await s3.listBuckets().promise();
         const fetchBucketTagsPromises = [];
+        const fetchBucketRegionPromises = [];
         listOfBuckets = listOfBuckets.Buckets;
         listOfBuckets.forEach(bucket => {
-            fetchBucketTagsPromises.push(s3.getBucketTagging({Bucket: bucket.Name}).promise());
+            const bucketParams = {Bucket: bucket.Name};
+            fetchBucketTagsPromises.push(s3.getBucketTagging(bucketParams).promise());
+            fetchBucketRegionPromises.push(s3.getBucketLocation(bucketParams).promise());
         })
         const listOfBucketTags = await Promise.all(fetchBucketTagsPromises);
+        const listOfBucketRegions = await Promise.all(fetchBucketRegionPromises);
         for (let index = 0; index < listOfBucketTags.length; index++) {
             listOfBuckets[index][tagSetKey] = listOfBucketTags[index][tagSetKey];
+            listOfBuckets[index][bucketRegionKey] = listOfBucketRegions[index][awsResponseBucketRegionKey];
         }
         return listOfBuckets;
     }
