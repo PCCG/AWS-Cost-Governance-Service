a5172b1f9f260983c6db4aaf35d8760e5c1a5eef
S Ajith Kumar <aka434112@gmail.com>
2020-05-05 02:52:17 +0530

    Modifying the endpoint to access the Cost Explorer service

--- routes/awsCostExplorer.js
+++ routes/awsCostExplorer.js
@@ -10,14 +10,14 @@ router.use((req, res, next) => {
     awsSvc.validateIfAwsCredsPresent(req, res, next, AWS.createNewCostExplorerObject);
 })
 
-router.post('/cost/ec2/getSpendPattern', async (req, res) => {
+router.post('/cost/getSpendPattern', async (req, res) => {
     const costExplorerServiceFilter = req.body.costExplorerServiceFilter;
     const ceServiceObject = res.locals.serviceObject;
     const spendPattern = await awsCeSvc.getCostPattern(ceServiceObject, costExplorerServiceFilter);
     res.send(spendPattern);
 })
 
-router.post('/cost/ec2/getSpendCurrentMonth', async (req, res) => {
+router.post('/cost/getSpendCurrentMonth', async (req, res) => {
     const costExplorerServiceFilter = req.body.costExplorerServiceFilter;
     const ceServiceObject = res.locals.serviceObject;
     const spendForTheCurrentMonth = awsCeSvc.getCostCurrentMonth(ceServiceObject, costExplorerServiceFilter);
