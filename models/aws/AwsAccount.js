9e41fb310e6d246e5c2e16a897465cc8c8ac02d0
S Ajith Kumar <ajithsjce@gmail.com>
2020-09-01 11:16:54 +0530

    feat(integrations): Store AWS accounts in MongoDB

---
+++ models/aws/AwsAccount.js
@@ -0,0 +1,39 @@
+const Schema = require('mongoose').Schema;
+const Model = require('mongoose').model;
+
+const AwsAccountSchema = new Schema({
+    accessKeyId: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    secretAccessKey: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    aliasName: {
+        type: String,
+        required: true
+    },
+    pollingInterval: {
+        required: true,
+        type: Number
+    },
+    reportName: {
+        type: String,
+        required: true
+    },
+    s3Bucket: {
+        type: String,
+        required: true
+    },
+    reportPrefix: {
+        type: String,
+        required: true
+    }
+});
+
+const AwsAccountModel = Model('AwsAccount', AwsAccountSchema);
+
+module.exports = AwsAccountModel;
--- routes/aws/Accounts.js
+++ routes/aws/Accounts.js
@@ -2,26 +2,44 @@ const express = require('express');
 const router = express.Router();
 const esClient = require("../../services/rest/elasticSearchSvc");
 
+const AwsAccount = require("../../models/aws/AwsAccount");
+
 //Map the object that houses account related information to a model.
 router.post('/credentials/postAccount', async function(req, res) {
-    if(req.body.credentials){
-        const credentialsObj = req.body.credentials;
-        const transactionData = await esClient.postAccountDetails(credentialsObj.accessKeyId, credentialsObj);
-        res.status(transactionData.status).send(transactionData.statusText);
-    } else {
-        res.status(400).send("The request is missing the required credentials");
+    try {
+        if(req.body.account){
+            const credentialsObj = req.body.account;
+            const awsAccount = new AwsAccount(credentialsObj);
+            await awsAccount.save();
+            await esClient.postAccountDetails(awsAccount._id, credentialsObj);
+            res.status(201).send();
+        } else {
+            res.status(400).send("The request is missing the required credentials");
+        }
+    } catch (e) {
+        console.log(e);
+        res.status(500).send(e.message);
     }
 })
 
 router.get('/credentials/getAccounts', async function(req, res){
-    const awsAccounts = await esClient.getAccountDetails()
-    res.send(awsAccounts);
+    try {
+        const listOfAwsAccounts = await AwsAccount.find();
+        res.send(listOfAwsAccounts);
+    } catch (e) {
+        res.stauts(500).send(e.message);
+    }
 })
 
 router.post('/credentials/deleteAccount', async (req, res) => {
-    const accessKeyId = req.body.accessKeyId;
-    await esClient.deleteAccount(accessKeyId);
-    res.send();
+    try {
+        const accountId = req.body.accountId;
+        await AwsAccount.findByIdAndDelete(accountId);
+        await esClient.deleteAccount(accountId);
+        res.send();
+    } catch (e) {
+        res.status(500).send(e.message);
+    }
 })
 
 module.exports = router;
